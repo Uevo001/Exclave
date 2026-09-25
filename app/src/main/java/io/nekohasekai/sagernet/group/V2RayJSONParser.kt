762206008f87e379138bc60c522e9cbcab02d72c
Thylis Voraan <69509738+Uevo001@users.noreply.github.com>
2026-09-25 22:52:51 +0800

    Refactor V2RayJSONParser by removing redundant conditions
    Removed unnecessary checks for 'reality' security and specific conditions for VLESSBean.

--- app/src/main/java/io/nekohasekai/sagernet/group/V2RayJSONParser.kt
+++ app/src/main/java/io/nekohasekai/sagernet/group/V2RayJSONParser.kt
@@ -1039,16 +1039,6 @@ fun parseV2RayOutbound(outbound: JsonObject): List<AbstractBean> {
                     }
                 }
             }
-            if (v2rayBean.security == "reality") {
-                when (v2rayBean.type) {
-                    "tcp", "http", "grpc", "splithttp" -> {}
-                    else -> return listOf()
-                }
-            }
-            if (v2rayBean is VLESSBean && v2rayBean.security != "none"&& v2rayBean.flow == "xtls-rprx-vision-udp443"
-                && v2rayBean.type != "tcp" && v2rayBean.encryption == "none") {
-                return listOf()
-            }
             return listOf(v2rayBean)
         }
         "hysteria2" -> {
